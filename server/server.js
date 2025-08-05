b2c667e32a524e95e11b0fabb27d028db4925d0b
Md Mahmudur Rahman <mdmahmudurrahman648@gmail.com>
2025-08-05 15:23:28 +0600

    update

--- server/server.js
+++ server/server.js
@@ -1,17 +1,29 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import connectDB from "./configs/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 // initialize express app
 const app = express();
+
+// connect database
+await connectDB()
+
+
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
 app.get ('/', (req,res)=> res.send("server is running"))
+app.use('/api/user', userRouter)
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=> console.log(`server running on port ${PORT}`))
+
+
+
+
 
