18d8ccca249f15e3facd5d8f20dcceadd6863d92
Md Mahmudur Rahman <mdmahmudurrahman648@gmail.com>
2025-08-05 18:56:31 +0600

    update

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -67,7 +67,20 @@ res.json({success:true, token})
 }
 
 // get user data using token (jwt)
+export const getUserData = async (req,res) => {
 
+    try {
+        const {user} = req;
+        res.json({success:true, user})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success:false, message:error.message})
+
+        
+        
+    }
+
+}
 
 
 
